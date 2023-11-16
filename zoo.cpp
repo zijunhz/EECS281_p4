e31f809bf8322688d9da75fd585a695b2dd8a532
Zijun Yang <zijunhz@126.com>
2023-11-16 14:51:18 -0500

    get 79.32/100

--- zoo.cpp
+++ zoo.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <limits>
 #include <list>
+#include <numeric>
 #include <vector>
 #define doubleInf numeric_limits<double>::infinity()
 using namespace std;
@@ -72,192 +73,218 @@ class Cage {
     void print() { cout << "    Cage(" << x << "," << y << ") type: " << type << "\n"; }
 };
 
+class Solution {
+   public:
+    vector<Cage> cages;
+    vector<CagePair> mstRes;
+    vector<uint16_t> pathRes;
+    vector<uint16_t> path;
+    double bestLen;
+    double curLen;
+    template <typename Dist, typename DistSq>
+    double mst(uint16_t startIndex, uint16_t endIndex, Dist dist, DistSq distSq, bool needRes) {
+        // index mapped to i-startIndex
+        // [startIndex,endIndex)
+        if (startIndex == endIndex)
+            return 0;
+        if (needRes)
+            mstRes.reserve(endIndex - startIndex);
+        vector<uint16_t> pre(endIndex - startIndex, 0);
+        vector<double> dis(endIndex - startIndex, doubleInf);
+        vector<bool> vis(endIndex - startIndex, false);
+        dis[0] = 0;
+        double len = 0;
+        for (uint16_t count = 0; count < endIndex - startIndex; ++count) {
+            double minDis = doubleInf;
+            uint16_t minNode = 0;
+            for (uint16_t j = 0; j < endIndex - startIndex; ++j)
+                if ((!vis[j]) && dis[j] < minDis) {
+                    minDis = dis[j];
+                    minNode = j;
+                }
+            if (minDis == doubleInf) {
+                cerr << "Cannot construct MST";
+                exit(1);
+            }
+            vis[minNode] = true;
+            if (count > 0) {
+                if (needRes)
+                    mstRes.push_back(CagePair(path[minNode + startIndex], path[pre[minNode] + startIndex]));
+                len += dist(cages[path[minNode + startIndex]], cages[path[pre[minNode] + startIndex]]);
+            }
+            for (uint16_t j = 0; j < endIndex - startIndex; ++j) {
+                if (vis[j])
+                    continue;
+                double newDis = distSq(cages[path[startIndex + j]], cages[path[startIndex + minNode]]);
+                if (newDis < dis[j]) {
+                    pre[j] = minNode;
+                    dis[j] = newDis;
+                }
+            }
+#ifdef DEBUG_MST
+            cout << "--------\n";
+            for (uint16_t i = 0; i < endIndex - startIndex; ++i) {
+                cout << "    " << dis[i] << "\n";
+            }
+#endif
+        }
+        return len;
+    }
+
+    bool promising(uint16_t permLength) {
+        // return true;
+        // return curLen < bestLen;
+        double min1 = doubleInf, min2 = doubleInf;
+        Cage::Mix dist;
+        for (uint16_t i = permLength; i < cages.size(); ++i) {
+            min1 = min(min1, dist(cages[path[0]], cages[path[i]]));
+            min2 = min(min2, dist(cages[path[permLength - 1]], cages[path[i]]));
+        }
+#ifdef DEBUG_PROMISING
+        if (true)
+            return curLen + min1 + min2 < bestLen;
+#endif
+        if (cages.size() < permLength + 6U)
+            return curLen + min1 + min2 < bestLen;
+        vector<CagePair> temp;
+        double mstLen = mst(permLength, uint16_t(cages.size()), Cage::Mix(), Cage::MixSq(), false);
+        return curLen + min1 + min2 + mstLen < bestLen;
+    }
+
+    void genPerms(uint16_t permLength) {
+        Cage::Mix dist;
+        if (permLength == path.size()) {
+            // Do something with the path
+            curLen += dist(cages[path[0]], cages[path.back()]);
+            // check the path and see if it's better
+            if (curLen < bestLen) {
+                bestLen = curLen;
+                pathRes = path;
+            }
+            curLen -= dist(cages[path[0]], cages[path.back()]);
+            return;
+        }  // if ..complete path
+
+        if (!promising(permLength)) {
+            return;
+        }  // if ..not promising
+
+        // if estimate > opt, cut
+        // do mst on the rest
+        // only calc estimate when # unvisited >= 5
+
+        for (uint16_t i = permLength; i < path.size(); ++i) {
+            swap(path[permLength], path[i]);
+            curLen += dist(cages[path[permLength]], cages[path[permLength - 1]]);
+            // stop genPerm
+            genPerms(permLength + 1);
+            curLen -= dist(cages[path[permLength]], cages[path[permLength - 1]]);
+            swap(path[permLength], path[i]);
+        }  // for ..unpermuted elements
+    }      // genPerms()
+};
+
 // this is for part B: https://stemlounge.com/animated-algorithms-for-the-traveling-salesman-problem/
 
-template <typename Dist, typename DistSq>
-double mst(vector<Cage>& cages,
-           uint16_t startIndex,
-           uint16_t endIndex,
-           vector<CagePair>& res,
-           Dist dist,
-           DistSq distSq) {
-    // index mapped to i-startIndex
-    // [startIndex,endIndex)
-    if (startIndex == endIndex)
-        return 0;
-    res.reserve(endIndex - startIndex);
-    vector<uint16_t> pre(endIndex - startIndex, 0);
-    vector<double> dis(endIndex - startIndex, doubleInf);
-    vector<bool> vis(endIndex - startIndex, false);
-    dis[0] = 0;
-    double len = 0;
-    for (uint16_t count = 0; count < endIndex - startIndex; ++count) {
+double farthest(vector<Cage>& cages, vector<uint16_t>& res) {
+    vector<bool> vis(cages.size(), false);
+    vector<uint16_t> nearest(cages.size(), 0);
+    list<uint16_t> path;
+    path.push_back(0);
+    path.push_back(0);
+    vis[0] = true;
+    Cage::MixSq distSq;
+    Cage::Mix dist;
+    for (uint16_t count = 1; count < cages.size(); ++count) {
+        uint16_t nxt = 0;
         double minDis = doubleInf;
-        uint16_t minNode = 0;
-        for (uint16_t j = 0; j < endIndex - startIndex; ++j)
-            if ((!vis[j]) && dis[j] < minDis) {
-                minDis = dis[j];
-                minNode = j;
-            }
-        if (minDis == doubleInf) {
-            cerr << "Cannot construct MST";
-            exit(1);
-        }
-        vis[minNode] = true;
-        if (count > 0) {
-            res.push_back(CagePair(minNode + startIndex, pre[minNode] + startIndex));
-            len += dist(cages[minNode + startIndex], cages[pre[minNode] + startIndex]);
-        }
-        for (uint16_t j = 0; j < endIndex - startIndex; ++j) {
-            if (vis[j])
+        for (uint16_t i = 1; i < cages.size(); ++i) {
+            if (vis[i])
                 continue;
-            double newDis = distSq(cages[startIndex + j], cages[startIndex + minNode]);
-            if (newDis < dis[j]) {
-                pre[j] = minNode;
-                dis[j] = newDis;
+            double distNow = distSq(cages[i], cages[nearest[i]]);
+            if (distNow < minDis) {
+                minDis = distNow;
+                nxt = i;
             }
         }
-#ifdef DEBUG_MST
-        cout << "--------\n";
-        for (uint16_t i = 0; i < endIndex - startIndex; ++i) {
-            cout << "    " << dis[i] << "\n";
+        vis[nxt] = true;
+        for (uint16_t i = 1; i < cages.size(); ++i) {
+            if ((!vis[i]) && distSq(cages[i], cages[nearest[i]]) < distSq(cages[i], cages[nxt])) {
+                nearest[i] = nxt;
+            }
         }
-#endif
+        auto toInsert = path.begin();
+        minDis = doubleInf;
+        for (auto it = path.begin(); next(it) != path.end(); ++it) {
+            double curDis = dist(cages[*it], cages[nxt]) + dist(cages[*(next(it))], cages[nxt]) -
+                            dist(cages[*it], cages[*next(it)]);
+            if (curDis < minDis) {
+                minDis = curDis;
+                toInsert = it;
+            }
+        }
+        path.insert(next(toInsert), nxt);
     }
+    double len = 0;
+    res.reserve(cages.size() + 1);
+    for (auto v : path) {
+        if (res.size() > 0)
+            len += dist(cages[res.back()], cages[v]);
+        res.push_back(v);
+    }
+    res.pop_back();
     return len;
 }
 
 double nearestInsert(vector<Cage>& cages, vector<uint16_t>& res) {
-    vector<bool> isIn(cages.size(), false);
-    res.resize(cages.size() + 1);
-    Cage::Mix dist;
-    Cage::MixSq distSq;
-    if (cages.size() > 1) {
-        double minDis = doubleInf;
-        uint16_t cage = 1;
-        for (uint16_t i = 1; i < cages.size(); ++i) {
-            double nowDis = distSq(cages[0], cages[i]);
-            if (nowDis < minDis) {
-                minDis = nowDis;
-                cage = i;
-            }
-        }
-        isIn[0] = true;
-        isIn[cage] = true;
-        res[0] = 0;
-        res[1] = cage;
-        res[2] = 0;
-    }
+    vector<bool> vis(cages.size(), false);
     vector<uint16_t> nearest(cages.size(), 0);
-    for (uint16_t i = 1; i < cages.size(); ++i) {
-        if (!isIn[i]) {
-            nearest[i] = distSq(cages[i], cages[0]) < distSq(cages[i], cages[res[1]]) ? 0 : res[1];
-        }
-    }
-
-    for (uint16_t count = 2; count < cages.size(); ++count) {
+    list<uint16_t> path;
+    path.push_back(0);
+    path.push_back(0);
+    vis[0] = true;
+    Cage::MixSq distSq;
+    Cage::Mix dist;
+    for (uint16_t count = 1; count < cages.size(); ++count) {
+        uint16_t nxt = 0;
         double minDis = doubleInf;
-        uint16_t cage = 1;
         for (uint16_t i = 1; i < cages.size(); ++i) {
-            if (isIn[i])
+            if (vis[i])
                 continue;
-            double nowDis = distSq(cages[i], cages[nearest[i]]);
-            if (nowDis < minDis) {
-                minDis = nowDis;
-                cage = i;
+            double distNow = distSq(cages[i], cages[nearest[i]]);
+            if (distNow < minDis) {
+                minDis = distNow;
+                nxt = i;
             }
         }
-        double minCost = doubleInf;
-        uint16_t insertBefore = 0;
-        for (uint16_t i = count; i > 0; --i) {
-            double nowCost = dist(cages[res[i]], cages[cage]) + dist(cages[res[i - 1]], cages[cage]) -
-                             dist(cages[res[i]], cages[res[i - 1]]);
-            if (nowCost < minCost) {
-                minCost = nowCost;
-                insertBefore = i;
-            }
-        }
-        for (uint16_t i = count + 1; i > insertBefore; --i) {
-            res[i] = res[i - 1];
-        }
-        res[insertBefore] = cage;
-        isIn[cage] = true;
+        vis[nxt] = true;
         for (uint16_t i = 1; i < cages.size(); ++i) {
-            if ((!isIn[i]) && distSq(cages[cage], cages[i]) < distSq(cages[i], cages[nearest[i]])) {
-                nearest[i] = cage;
+            if ((!vis[i]) && distSq(cages[i], cages[nearest[i]]) > distSq(cages[i], cages[nxt])) {
+                nearest[i] = nxt;
             }
         }
+        auto toInsert = path.begin();
+        minDis = doubleInf;
+        for (auto it = path.begin(); next(it) != path.end(); ++it) {
+            double curDis = dist(cages[*it], cages[nxt]) + dist(cages[*(next(it))], cages[nxt]) -
+                            dist(cages[*it], cages[*next(it)]);
+            if (curDis < minDis) {
+                minDis = curDis;
+                toInsert = it;
+            }
+        }
+        path.insert(next(toInsert), nxt);
     }
-
-    // for (uint16_t i = 1; i < cages.size(); ++i) {
-    //     for (uint16_t j = i + 2; j < cages.size(); ++j) {
-    //         if (dist(cages[res[i - 1]], cages[res[i]]) + dist(cages[res[i + 1]], cages[res[i]]) +
-    //                 dist(cages[res[j - 1]], cages[res[j]]) + dist(cages[res[j + 1]], cages[res[j]]) >
-    //             dist(cages[res[i - 1]], cages[res[j]]) + dist(cages[res[i + 1]], cages[res[j]]) +
-    //                 dist(cages[res[j - 1]], cages[res[i]]) + dist(cages[res[j + 1]], cages[res[i]]))
-    //             swap(res[i], res[j]);
-    //     }
-    // }
-
     double len = 0;
-    for (uint16_t i = 0; i < cages.size(); ++i)
-        len += dist(cages[res[i]], cages[res[i + 1]]);
+    res.reserve(cages.size() + 1);
+    for (auto v : path) {
+        if (res.size() > 0)
+            len += dist(cages[res.back()], cages[v]);
+        res.push_back(v);
+    }
+    res.pop_back();
     return len;
 }
-
-bool promising(vector<Cage>& cages, vector<uint16_t>& path, uint16_t permLength, double curLen, double& bestLen) {
-    double min1 = doubleInf, min2 = doubleInf;
-    Cage::Mix dist;
-    for (uint16_t i = permLength; i < cages.size(); ++i) {
-        min1 = min(min1, dist(cages[path[0]], cages[path[i]]));
-        min2 = min(min2, dist(cages[path[permLength - 1]], cages[path[i]]));
-    }
-#ifdef DEBUG_PROMISING
-    if (true)
-        return curLen + min1 + min2 < bestLen;
-#endif
-    if (cages.size() < permLength + 6U)
-        return curLen + min1 + min2 < bestLen;
-    vector<CagePair> temp;
-    double mstLen = mst(cages, permLength, uint16_t(cages.size()), temp, Cage::Mix(), Cage::MixSq());
-    return curLen + min1 + min2 + mstLen < bestLen;
-}
-
-void genPerms(vector<Cage>& cages,
-              vector<uint16_t>& ans,
-              double& bestLen,
-              vector<uint16_t>& path,
-              double curLen,
-              uint16_t permLength) {
-    Cage::Mix dist;
-    if (permLength == path.size()) {
-        // Do something with the path
-        curLen += dist(cages[path[0]], cages[path.back()]);
-        // check the path and see if it's better
-        if (curLen < bestLen) {
-            bestLen = curLen;
-            ans = path;
-        }
-        return;
-    }  // if ..complete path
-
-    if (!promising(cages, path, permLength, curLen, bestLen)) {
-        return;
-    }  // if ..not promising
-
-    // if estimate > opt, cut
-    // do mst on the rest
-    // only calc estimate when # unvisited >= 5
-
-    for (uint16_t i = permLength; i < path.size(); ++i) {
-        swap(path[permLength], path[i]);
-        curLen += dist(cages[path[permLength]], cages[path[permLength - 1]]);
-        // stop genPerm
-        genPerms(cages, ans, bestLen, path, curLen, permLength + 1);
-        curLen -= dist(cages[path[permLength]], cages[path[permLength - 1]]);
-        swap(path[permLength], path[i]);
-    }  // for ..unpermuted elements
-}  // genPerms()
 
 int main(int argc, char** argv) {
     ios_base::sync_with_stdio(false);
@@ -297,38 +324,42 @@ int main(int argc, char** argv) {
     }
     uint16_t n;
     cin >> n;
-    vector<Cage> cages;
-    cages.reserve(n);
+    Solution sol;
+    sol.cages.reserve(n);
     for (uint16_t i = 0; i < n; ++i) {
         int x, y;
         cin >> x >> y;
-        cages.push_back(Cage(x, y));
+        sol.cages.push_back(Cage(x, y));
 #ifdef DEBUG_INPUT
         cages.back().print();
 #endif
     }
     if (mode == Mode::MST) {
         vector<CagePair> res;
-        double len = mst(cages, 0, n, res, Cage::Distinguish(), Cage::DistinguishSq());
+        sol.path.resize(n);
+        iota(sol.path.begin(), sol.path.end(), 0);
+        double len = sol.mst(0, n, Cage::Distinguish(), Cage::DistinguishSq(), true);
         cout << len << "\n";
         for (int i = 0; i < n - 1; ++i) {
-            cout << min(res[i].i, res[i].j) << " " << max(res[i].i, res[i].j) << '\n';
+            cout << min(sol.mstRes[i].i, sol.mstRes[i].j) << " " << max(sol.mstRes[i].i, sol.mstRes[i].j) << '\n';
         }
     } else if (mode == Mode::FASTTSP) {
         vector<uint16_t> res;
-        double len = nearestInsert(cages, res);
+        double len = nearestInsert(sol.cages, res);
         cout << len << '\n';
         for (uint16_t i = 0; i < n; ++i)
             cout << res[i] << ' ';
     } else {
         vector<uint16_t> res;
-        double len = nearestInsert(cages, res);
-        vector<uint16_t> temp(res);
-        temp.pop_back();
-        genPerms(cages, res, len, temp, 0, 1);
-        cout << len << '\n';
+        double len = nearestInsert(sol.cages, res);
+        sol.bestLen = len;
+        sol.curLen = 0;
+        sol.path = res;
+        sol.pathRes = res;
+        sol.genPerms(1);
+        cout << sol.bestLen << '\n';
         for (uint16_t i = 0; i < n; ++i)
-            cout << res[i] << ' ';
+            cout << sol.pathRes[i] << ' ';
     }
 
     // in part C, call part B first to be the starting point of part C.
