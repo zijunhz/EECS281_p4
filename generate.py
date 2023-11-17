ff7964d687374e6ef9c77f51417742f9deda347a
Zijun Yang <zijunhz@126.com>
2023-11-16 20:17:41 -0500

    update generator

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ from random import randint
 
 n = 10
 print(n)
-print("0 1")
-print("-7 0")
+print(f"0 {randint(-10,0)}")
+print(f"{randint(-10,0)} 0")
 for i in range(2, n):
     print(f"{randint(-10,10)} {randint(-10,10)}")
