18da00f56f7e7cf03ac229dea43400ccaacbac79
Zijun Yang <zijunhz@126.com>
2023-11-17 17:22:27 -0500

    modify generator

--- generate.py
+++ generate.py
@@ -1,13 +1,31 @@
 from random import randint
 
-for count in range(0, 10):
-    with open(
-        f'./tests/test-{count}-{"MST" if count<4 else "OPTTSP" if count<8 else "FASTTSP"}.txt',
-        "w",
-    ) as file:
-        n = 25
-        file.write(f"{n}\n")
-        file.write(f"0 {randint(-10,0)}\n")
-        file.write(f"{randint(-10,0)} 0\n")
-        for i in range(2, n):
-            file.write(f"{randint(-10,10)} {randint(-10,10)}\n")
+print(
+    "enter 0 to generate testcases with n=10, enter other to generate those with n=25:"
+)
+size = input()
+
+if size == "0":
+    for count in range(0, 10):
+        with open(
+            f'./tests/test-{count}-{"MST" if count<4 else "OPTTSP" if count<8 else "FASTTSP"}.txt',
+            "w",
+        ) as file:
+            n = 10
+            file.write(f"{n}\n")
+            file.write(f"0 {randint(-10,0)}\n")
+            file.write(f"{randint(-10,0)} 0\n")
+            for i in range(2, n):
+                file.write(f"{randint(-10,10)} {randint(-10,10)}\n")
+else:
+    for count in range(0, 10):
+        with open(
+            f"./tests/large_test-{count}-OPTTSP.txt",
+            "w",
+        ) as file:
+            n = 25
+            file.write(f"{n}\n")
+            file.write(f"0 {randint(-100,0)}\n")
+            file.write(f"{randint(-100,0)} 0\n")
+            for i in range(2, n):
+                file.write(f"{randint(-100,100)} {randint(-100,100)}\n")
