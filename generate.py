f48d4bf7f77c4982fcfd66d3fed12e30a9b7cb40
Zijun Yang <zijunhz@126.com>
2023-11-16 20:32:06 -0500

    modify generator

--- generate.py
+++ generate.py
@@ -1,8 +1,13 @@
 from random import randint
 
-n = 10
-print(n)
-print(f"0 {randint(-10,0)}")
-print(f"{randint(-10,0)} 0")
-for i in range(2, n):
-    print(f"{randint(-10,10)} {randint(-10,10)}")
+for count in range(0, 10):
+    with open(
+        f'./tests/test-{count}-{"MST" if count<4 else "OPTTSP" if count<8 else "FASTTSP"}.txt',
+        "w",
+    ) as file:
+        n = 10
+        file.write(f"{n}\n")
+        file.write(f"0 {randint(-10,0)}\n")
+        file.write(f"{randint(-10,0)} 0\n")
+        for i in range(2, n):
+            file.write(f"{randint(-10,10)} {randint(-10,10)}\n")
